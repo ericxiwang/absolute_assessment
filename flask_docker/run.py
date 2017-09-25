8816364b428c8bd97be57bca2785dbfb43f40d04
fredriksson <eric@erics-MacBook-Pro.local>
2017-09-24 20:28:35 -0700

    add another solution based on docker

---
+++ flask_docker/run.py
@@ -0,0 +1,7 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World from Flask in a uWSGI Nginx Docker container with \
+     Python 2.7 (default)"
